541cd4e3dffad541beacdc801e56747cd30a10be
HafsahJoo <hafsah260103@gmail.com>
2024-09-28 22:56:08 +0400

    refactor: Update Header component styles and structure

--- my-portfolio/src/components/Header.vue
+++ my-portfolio/src/components/Header.vue
@@ -1,15 +1,17 @@
 <template>
-<header class="border-b-2 border-x-0 border-gray-300 flex dark:bg-black flex-row items-center justify-between p-2">
-    <div class="mx-8">
-        <button>
-            <h1 class="font-lemonmilk text-lightGray font-light">Hafsah</h1>
-        </button>
-    </div>
-    <div class="mx-8">
-        <button class="border-gray-300 rounded border-2 p-1 dark:bg-gray-600" @click="toggleDarkMode">
-            <img v-if="isDarkMode" src="../assets/icons/lightmode.svg" class="w-6 h-6" alt="Light Mode" />
-            <img v-else src="../assets/icons/darkmode.svg" class="w-6 h-6" alt="Dark Mode" />
-        </button>
+<header class="border-b-2 border-x-0 border-gray-300  dark:bg-black flex flex-row items-center justify-center">
+    <div class=" w-full max-w-[2500px] flex flex-row items-center justify-between p-2">
+        <div class="mx-8">
+            <button>
+                <h1 class="font-lemonmilk text-lightGray font-light">Hafsah</h1>
+            </button>
+        </div>
+        <div class="mx-8">
+            <button class="border-gray-300 rounded border-2 p-1 dark:bg-gray-600" @click="toggleDarkMode">
+                <img v-if="isDarkMode" src="../assets/icons/lightmode.svg" class="w-6 h-6" alt="Light Mode" />
+                <img v-else src="../assets/icons/darkmode.svg" class="w-6 h-6" alt="Dark Mode" />
+            </button>
+        </div>
     </div>
 </header>
 
