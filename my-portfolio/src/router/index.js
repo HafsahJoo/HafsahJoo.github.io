e0f618d494f9a7a8508a1f662e2b63db540374ba
HafsahJoo <hafsah260103@gmail.com>
2024-09-29 17:13:35 +0400

    refactor: Update router to use createWebHistory instead of createWebHashHistory

--- my-portfolio/src/router/index.js
+++ my-portfolio/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory  } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../components/HomePage.vue";
 import ProjectPage from "../components/ProjectPage.vue";
 import BlogPage from "../components/BlogPage.vue";
@@ -26,7 +26,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes,
 });
 
